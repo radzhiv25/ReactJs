78066e55b91ab1f0e1b32ad69a9d86b4e81583b9
Rajeev Krishna <lambo2572@gmail.com>
2023-06-02 23:28:54 +0530

    Portfolio work started

---
+++ portfolio/src/components/Navbar.js
@@ -0,0 +1,14 @@
+export const Navbar = () => {
+    return(
+        <div className="nav">
+            <h1>Radzhiv</h1>
+            <ul>
+                <li>About</li>
+                <li>Projects</li>
+                <li>Contact</li>
+                <li><button>Resume</button></li>
+                <li>Dark</li>
+            </ul>
+        </div>
+    )
+}
